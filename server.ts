7c2cd5499d29249510c3b42db0dc6d19cd57743e
IsaharStokhammer <8526656@gmail.com>
2024-09-26 17:26:11 +0300

    קושר לפרונט

--- server.ts
+++ server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from "dotenv";
 import crudRouter from "./routes/crud.js"
+import cors from 'cors';
 dotenv.config();
+
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(express.json()); // Body parser
+app.use(cors());
 
 app.use('/home', (req, res) => {
     res.send("Hello World😎");
